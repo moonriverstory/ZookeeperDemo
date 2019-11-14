2bb5ec1725f8d87329b2c2fef3e154e0ab983eed
rain <rainnight@outlook.com>
2019-11-14 17:48:32 +0800

    zk curator acl demo

--- src/main/java/curator/ZKCuratorUtil.java
+++ src/main/java/curator/ZKCuratorUtil.java
@@ -69,22 +69,24 @@ public class ZKCuratorUtil {
                 CuratorFrameworkFactory.builder()
                         .aclProvider(aclProvider)
                         .connectString(url)
+                        .namespace(namespace)
                         .authorization("digest", (user + ":" + passwd).getBytes()) //设置scheme digest授权
                         .retryPolicy(new ExponentialBackoffRetry(1000, 5))  //重试策略
                         .build();
         curatorFramework.start();
-        try {
-            curatorFramework.blockUntilConnected(3, TimeUnit.SECONDS); //阻塞判断连接成功，超时认为失败。
-            if (curatorFramework.getZookeeperClient().isConnected()) {
-                return curatorFramework.usingNamespace(namespace); //返回连接，起始根目录为namespace。
-            } else {
-                curatorFramework.close();
-                throw new RuntimeException("failed to connect to zookeeper service : " + url);
-            }
-        } catch (Exception e) {
-            curatorFramework.close();
-            throw new RuntimeException("failed to connect to zookeeper service : " + url + " error: " + e.getMessage());
-        }
+        return curatorFramework;
+//        try {
+//            curatorFramework.blockUntilConnected(3, TimeUnit.SECONDS); //阻塞判断连接成功，超时认为失败。
+//            if (curatorFramework.getZookeeperClient().isConnected()) {
+//                return curatorFramework.usingNamespace(namespace); //返回连接，起始根目录为namespace。
+//            } else {
+//                curatorFramework.close();
+//                throw new RuntimeException("failed to connect to zookeeper service : " + url);
+//            }
+//        } catch (Exception e) {
+//            curatorFramework.close();
+//            throw new RuntimeException("failed to connect to zookeeper service : " + url + " error: " + e.getMessage());
+//        }
     }
 
     /**
@@ -150,44 +152,64 @@ public class ZKCuratorUtil {
      * @throws Exception
      */
     public static void demoProcess2() throws Exception {
-        CuratorFramework client = ZKCuratorUtil.getDigestConnect(CONNECTION_URL,"","admin","12345");
+        CuratorFramework client = ZKCuratorUtil.getDigestConnect(CONNECTION_URL,"dsf","admin","12345");
 
         //2-------
         //读取 /
         byte[] pathRoot = client.getData().forPath("/");
         System.out.println("path / data: " + new String(pathRoot));
         //读取 /dsf/service
-        byte[] pathDsf = client.getData().forPath("/dsf/service");
+        byte[] pathDsf = client.getData().forPath("/service");
         System.out.println("path /dsf/service data: " + new String(pathDsf));
         //获得 / 的所有子节点
         List<String> lsRoot = client.getChildren().forPath("/");
         System.out.println("ls /: " + lsRoot.toString());
 
         //检查节点是否存在
-        Stat exist = client.checkExists().forPath("/dsf/service");
+        Stat exist = client.checkExists().forPath("/service");
         System.out.println("is /dsf/service exist: " + (exist == null ? "false" : "true"));
-        Stat exist123 = client.checkExists().forPath("/dsf/service/123");
+        Stat exist123 = client.checkExists().forPath("/service/123");
         System.out.println("is /dsf/service/123 exist: " + (exist123 == null ? "false" : "true"));
 
         //创建节点:节点创建模式默认为持久化节点，内容默认为空
-        client.create().forPath("/dsf/service/123");
-        Stat exist2 = client.checkExists().forPath("/dsf/service/123");
+        client.create().forPath("/service/123");
+        Stat exist2 = client.checkExists().forPath("/service/123");
         System.out.println("is /dsf/service/123 exist: " + (exist2 == null ? "false" : "true"));
         //设置数据
-        client.setData().forPath("/dsf/service/123", "666".getBytes());
+        client.setData().forPath("/service/123", "666".getBytes());
         //读取
-        byte[] pathCreate = client.getData().forPath("/dsf/service/123");
+        byte[] pathCreate = client.getData().forPath("/service/123");
         System.out.println("pathCreate: get /dsf/service/123 data : " + new String(pathCreate));
         //删除节点
-        client.delete().forPath("/dsf/service/123");
-        Stat exist3 = client.checkExists().forPath("/dsf/service/123");
+        client.delete().forPath("/service/123");
+        Stat exist3 = client.checkExists().forPath("/service/123");
         System.out.println("is /dsf/service/123 exist: " + (exist3 == null ? "false" : "true"));
 
         client.close();
     }
+    /**
+     2019-11-14 17:46:23,965 INFO  [main] [ZooKeeper.java:438] : Initiating client connection, connectString=10.211.95.114:6830 sessionTimeout=60000 watcher=org.apache.curator.ConnectionState@54a097cc
+     2019-11-14 17:46:24,100 INFO  [main] [CuratorFrameworkImpl.java:356] : Default schema
+     2019-11-14 17:46:24,173 INFO  [main-SendThread(10.211.95.114:6830)] [ClientCnxn.java:975] : Opening socket connection to server 10.211.95.114/10.211.95.114:6830. Will not attempt to authenticate using SASL (unknown error)
+     2019-11-14 17:46:24,184 INFO  [main-SendThread(10.211.95.114:6830)] [ClientCnxn.java:852] : Socket connection established to 10.211.95.114/10.211.95.114:6830, initiating session
+     2019-11-14 17:46:24,197 INFO  [main-SendThread(10.211.95.114:6830)] [ClientCnxn.java:1235] : Session establishment complete on server 10.211.95.114/10.211.95.114:6830, sessionid = 0x16e43fc56a437d6, negotiated timeout = 60000
+     2019-11-14 17:46:24,203 INFO  [main-EventThread] [ConnectionStateManager.java:251] : State change: CONNECTED
+     path / data:
+     path /dsf/service data:
+     ls /: [service, instance]
+     is /dsf/service exist: true
+     is /dsf/service/123 exist: false
+     is /dsf/service/123 exist: true
+     pathCreate: get /dsf/service/123 data : 666
+     is /dsf/service/123 exist: false
+     2019-11-14 17:46:24,306 INFO  [Curator-Framework-0] [CuratorFrameworkImpl.java:955] : backgroundOperationsLoop exiting
+     2019-11-14 17:46:24,316 INFO  [main] [ZooKeeper.java:684] : Session: 0x16e43fc56a437d6 closed
+     2019-11-14 17:46:24,316 INFO  [main-EventThread] [ClientCnxn.java:512] : EventThread shut down
+     */
+
 
     public static void main(String[] args) throws Exception {
-        demoProcess1();
-        //demoProcess2();
+        //demoProcess1();
+        demoProcess2();
     }
 }
